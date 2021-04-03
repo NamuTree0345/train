c5d0b14b5206916fe0aa67c4ba9ac7e9c7990ee8
TreeKR <johanbag290@gmail.com>
2021-04-03 14:40:36 +0900

    Apply TabCompleter

--- src/main/kotlin/xyz/namutree0345/train/Train.kt
+++ src/main/kotlin/xyz/namutree0345/train/Train.kt
@@ -8,8 +8,14 @@ val trainMap: HashMap<Player, Player> = HashMap()
 class Train : JavaPlugin() {
 
     override fun onEnable() {
-        getCommand("train")?.setExecutor(TrainCommand())
-        getCommand("trainrelease")?.setExecutor(TrainReleaseCommand())
+        getCommand("train")?.also {
+            it.setExecutor(TrainCommand())
+            it.tabCompleter = PlayerListCommandCompleter()
+        }
+        getCommand("trainrelease")?.also {
+            it.setExecutor(TrainReleaseCommand())
+            it.tabCompleter = PlayerListCommandCompleter()
+        }
     }
 
 }
